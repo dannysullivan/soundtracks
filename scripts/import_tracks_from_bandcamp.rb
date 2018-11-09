aae98cf3de1db0e9499bddd488318e217c66f4ae
Danny Sullivan <dsullivan@pivotal.io>
2018-11-09 10:57:27 -0500

    Add tags via data import script

--- scripts/import_tracks_from_bandcamp.rb
+++ scripts/import_tracks_from_bandcamp.rb
@@ -1,5 +1,9 @@
 require 'CSV'
 
 CSV.foreach('tmp/tracks.csv', headers: true) do |line|
-  Track.find_or_create_by(title: line[0], bandcamp_track_id: line[1])
+  track = Track.find_or_create_by(title: line[0], bandcamp_track_id: line[1])
+  line[2].split(', ').each do |tag_name|
+    tag = Tag.find_or_create_by(name: tag_name)
+    track.update_attributes(tags: (track.tags + [tag]).uniq)
+  end
 end

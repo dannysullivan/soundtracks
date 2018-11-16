f4a64890ebf6efd9cb2774f282639e6465cefc2e
Danny Sullivan <dsullivan@pivotal.io>
2018-11-16 12:22:44 -0500

    Move tracks csv to config folder

--- scripts/import_tracks_from_bandcamp.rb
+++ scripts/import_tracks_from_bandcamp.rb
@@ -1,6 +1,6 @@
 require 'CSV'
 
-CSV.foreach('tmp/tracks.csv', headers: true) do |line|
+CSV.foreach('config/tracks.csv', headers: true) do |line|
   track = Track.find_or_create_by(title: line[0], bandcamp_track_id: line[1])
   line[2].split(', ').each do |tag_name|
     tag = Tag.find_or_create_by(name: tag_name)
